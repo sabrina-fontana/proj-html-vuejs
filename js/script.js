c48bdb5b0712509bea195dc11b313b1b9ef3d1af
sabrina-fontana <sabfon@hotmail.it>
2021-02-18 13:38:54 +0100

    inizio jumbotron

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var app = new Vue({
 el: '#app',
 data: {
-  headerLinks: ['home', 'about', 'services', 'showcase', 'blog', 'contact']
+  headerLinks: ['home', 'about', 'services', 'showcase', 'blog', 'contact'],
+  defaultText: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.'
   }
 });
 Vue.config.devtools = true;
