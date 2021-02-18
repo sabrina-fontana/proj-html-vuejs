c64c608b0124e48bae8d44f9ed54cc757a4d21cb
sabrina-fontana <sabfon@hotmail.it>
2021-02-18 16:04:09 +0100

    prima sezione

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ var app = new Vue({
 el: '#app',
 data: {
   headerLinks: ['home', 'about', 'services', 'showcase', 'blog', 'contact'],
-  defaultText: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.'
+  defaultText: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.',
+  defaultShortText: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated...'
   }
 });
 Vue.config.devtools = true;
