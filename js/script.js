01767ad48dfdf4b6f31d1ef89ebf88da03864544
sabrina-fontana <sabfon@hotmail.it>
2021-02-19 09:26:03 +0100

    seconda e terza sezione

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 var app = new Vue({
 el: '#app',
 data: {
-  headerLinks: ['home', 'about', 'services', 'showcase', 'blog', 'contact'],
+  headerLinks: [
+    {
+      name: 'home',
+      link: ''
+    },
+    {
+      name: 'about',
+      link: ''
+    },
+    {
+      name: 'services',
+      link: ''
+    },
+    {
+      name: 'showcase',
+      link: ''
+    },
+    {
+      name: 'blog',
+      link: ''
+    },
+    {
+      name: 'contact',
+      link: ''
+    }
+  ],
   defaultText: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.',
   defaultShortText: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated...'
   }
