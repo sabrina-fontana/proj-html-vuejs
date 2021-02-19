4e2036be13b3ea53083b9b4243a4b9ecb0420f2d
sabrina-fontana <sabfon@hotmail.it>
2021-02-19 15:13:06 +0100

    footer completo

--- js/script.js
+++ js/script.js
@@ -27,6 +27,32 @@ data: {
       link: ''
     }
   ],
+  footerLinks: [
+    {
+      name: 'Careers',
+      link: ''
+    },
+    {
+      name: 'News',
+      link: ''
+    },
+    {
+      name: 'Terms of use',
+      link: ''
+    },
+    {
+      name: 'Privacy Projects',
+      link: ''
+    },
+    {
+      name: 'About',
+      link: ''
+    },
+    {
+      name: 'Contact',
+      link: ''
+    }
+  ],
   defaultText: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.',
   defaultShortText: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated...'
   }
